cd081da36667f08c2d5af7e5f3fe0cee39cfde28
lassebuurrasmussen <wogie@lassebuur.dk>
2021-10-14 21:29:13 +0200

    Move DavidsenshopSpider definition to top

--- seven_mio_project/spiders/DavidsenShopSpider.py
+++ seven_mio_project/spiders/DavidsenShopSpider.py
@@ -6,6 +6,32 @@ from scrapy.selector import SelectorList
 from seven_mio_project.CustomExceptions import OutdatedError, UnexpectedResultError
 
 
+class DavidsenshopSpider(scrapy.Spider):
+    name = "DavidsenShopSpider"
+    start_urls = ["https://www.davidsenshop.dk/"]
+
+    def parse(self, response: TextResponse, **kwargs):
+        try:
+            byg_main_category_url = get_href_of_element_from_group(
+                response=response,
+                element_name="byg",
+                element_group_css_query="div.QuickBasketAndMenus__MenuWrapper-sc-17cnu5q-20.fQBxeN div ul li a",
+            )
+        except OutdatedError:
+            # Assume that byg page URL itself has not changed
+            byg_main_category_url = "https://www.davidsenshop.dk/byg-c-id497143"
+
+        yield response.follow(byg_main_category_url, callback=self.parse_main_category_page)
+
+    def parse_main_category_page(self, response: TextResponse):
+        tree_sub_category_url = get_href_of_element_from_group(
+            response=response, element_name="træ", element_group_css_query="div.sc-bxivhb.bHTkun div div ul li a"
+        )
+
+        # TODO: Follow tree_sub_category_url to get to lægter, so we can start scraping "Lægter" and "Reglar"
+        yield {"tree_sub_category_url": tree_sub_category_url}
+
+
 def get_href_of_element_from_group(response: TextResponse, element_name: str, element_group_css_query: str) -> str:
     """
     Raises:
@@ -46,28 +72,3 @@ def extract_all_element_group_texts(element_group_selector: SelectorList) -> dic
 
 def extract_all_lower_case_texts(selector: Selector) -> list[str]:
     return [element_text.lower() for element_text in selector.css("::text").getall()]
-
-
-class DavidsenshopSpider(scrapy.Spider):
-    name = "DavidsenShopSpider"
-    start_urls = ["https://www.davidsenshop.dk/"]
-
-    def parse(self, response: TextResponse, **kwargs):
-        try:
-            byg_main_category_url = get_href_of_element_from_group(
-                response=response,
-                element_name="byg",
-                element_group_css_query="div.QuickBasketAndMenus__MenuWrapper-sc-17cnu5q-20.fQBxeN div ul li a",
-            )
-        except OutdatedError:
-            # Assume that byg page URL itself has not changed
-            byg_main_category_url = "https://www.davidsenshop.dk/byg-c-id497143"
-
-        yield response.follow(byg_main_category_url, callback=self.parse_main_category_page)
-
-    def parse_main_category_page(self, response: TextResponse):
-        tree_sub_category_url = get_href_of_element_from_group(
-            response=response, element_name="træ", element_group_css_query="div.sc-bxivhb.bHTkun div div ul li a"
-        )
-
-        yield {"tree_sub_category_url": tree_sub_category_url}
